a04bb811110242255e851a2686fde726c2614b96
BlackRain <masaraksh86@mail.ru>
2025-02-17 19:26:28 +0700

    Добавлен метод "YouTubeVideoId.GetWebPage()"

--- YouTubeApiLib/YouTubeVideoId.cs
+++ YouTubeApiLib/YouTubeVideoId.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using MultiThreadedDownloaderLib;
+
 namespace YouTubeApiLib
 {
 	public class YouTubeVideoId
@@ -13,6 +14,11 @@ namespace YouTubeApiLib
 		public YouTubeVideo GetVideo(IYouTubeClient client = null)
 		{
 			return YouTubeVideo.GetById(this, client);
+		}
+
+		public YouTubeVideoWebPageResult GetWebPage(FileDownloader downloader = null)
+		{
+			return YouTubeVideoWebPage.Get(Id, downloader);
 		}
 
 		public override string ToString()
