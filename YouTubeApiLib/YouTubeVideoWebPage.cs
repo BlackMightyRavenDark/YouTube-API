ec0f4ddab8818576748c600313de499b37778dc7
BlackRain <masaraksh86@mail.ru>
2025-02-18 18:59:46 +0700

    Добавлены HTTP-заголовки по-умолчанию для скачивания веб-страницы видео

--- YouTubeApiLib/YouTubeVideoWebPage.cs
+++ YouTubeApiLib/YouTubeVideoWebPage.cs
@@ -1,4 +1,5 @@
 ﻿using MultiThreadedDownloaderLib;
+using System.Collections.Specialized;
 
 namespace YouTubeApiLib
 {
@@ -19,6 +20,28 @@ namespace YouTubeApiLib
 		internal static YouTubeVideoWebPageResult Get(string videoId, FileDownloader downloader = null)
 		{
 			string url = Utils.GetYouTubeVideoUrl(videoId);
+			if (downloader == null)
+			{
+				NameValueCollection headers = new NameValueCollection()
+				{
+					{ "Host", "www.youtube.com" },
+					{ "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0" },
+					{ "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" },
+					{ "Accept-Language", "en-US" },
+					{ "Accept-Encoding", "gzip" },
+					{ "DNT", "1" },
+					{ "Sec-GPC", "1" },
+					{ "Upgrade-Insecure-Requests", "1" },
+					{ "Sec-Fetch-Dest", "document" },
+					{ "Sec-Fetch-Mode", "navigate" },
+					{ "Sec-Fetch-Site", "none" },
+					{ "Sec-Fetch-User", "?1" },
+					{ "Priority", "u=0, i" }
+				};
+
+				downloader = new FileDownloader() { Headers = headers };
+			};
+
 			int errorCode = Utils.DownloadString(url, out string responseWebPageCode, downloader);
 			YouTubeVideoWebPage webPage = errorCode == 200 ?
 				new YouTubeVideoWebPage(new YouTubeVideoId(videoId), responseWebPageCode, false) : null;
