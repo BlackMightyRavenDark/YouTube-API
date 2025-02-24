e103417299d5fe96a56a95c420a7734c0e294444
BlackRain <masaraksh86@mail.ru>
2025-02-24 15:47:11 +0700

    Дополнен класс "YouTubeChannelTabPage()" - Добавлено свойство "UrlSuffix" - Добавлены методы "GetWebPageUrl()" и "GetWebPageUrlSuffix()"

--- YouTubeApiLib/YouTubeChannelTabPage.cs
+++ YouTubeApiLib/YouTubeChannelTabPage.cs
@@ -5,5 +5,16 @@ namespace YouTubeApiLib
 	{
 		public string Title { get; protected set; }
 		public string ParamsId { get; protected set; }
+		public string UrlSuffix { get; protected set; }
+
+		public virtual string GetWebPageUrl(string channelId)
+		{
+			return $"{Utils.YOUTUBE_URL}/channel/{channelId}/{GetWebPageUrlSuffix()}";
+		}
+
+		public virtual string GetWebPageUrlSuffix()
+		{
+			return !string.IsNullOrEmpty(UrlSuffix) ? UrlSuffix.ToLower() : Title.ToLower();
+		}
 	}
 }
--- YouTubeApiLib/YouTubeChannelTabPages.cs
+++ YouTubeApiLib/YouTubeChannelTabPages.cs
@@ -6,80 +6,88 @@ namespace YouTubeApiLib
 		public static readonly TabPageHome Home = new TabPageHome("Home", "EghmZWF0dXJlZA%3D%3D");
 		public static readonly TabPageVideos Videos = new TabPageVideos("Videos", "EgZ2aWRlb3MYAyAAMAE%3D");
 		public static readonly TabPageShorts Shorts = new TabPageShorts("Shorts", "EgZzaG9ydHPyBgUKA5oBAA%3D%3D");
-		public static readonly TabPageLive Live = new TabPageLive("Live", "EgdzdHJlYW1z8gYECgJ6AA%3D%3D");
+		public static readonly TabPageLive Live = new TabPageLive("Live", "EgdzdHJlYW1z8gYECgJ6AA%3D%3D", "streams");
 		public static readonly TabPagePlaylists Playlists = new TabPagePlaylists("Playlists", "EglwbGF5bGlzdHPyBgQKAkIA");
 		public static readonly TabPageCommunity Community = new TabPageCommunity("Community", "Egljb21tdW5pdHk%3D");
 	}
 
 	public class TabPageHome : YouTubeChannelTabPage
 	{
-		public TabPageHome(string title, string paramsId)
+		public TabPageHome(string title, string paramsId, string urlSuffix = null)
 		{
 			Title = title;
 			ParamsId = paramsId;
+			UrlSuffix = urlSuffix;
 		}
 	}
 
 	public class TabPageVideos : YouTubeChannelTabPage
 	{
-		public TabPageVideos(string title, string paramsId)
+		public TabPageVideos(string title, string paramsId, string urlSuffix = null)
 		{
 			Title = title;
 			ParamsId = paramsId;
+			UrlSuffix = urlSuffix;
 		}
 	}
 
 	public class TabPageShorts : YouTubeChannelTabPage
 	{
-		public TabPageShorts(string title, string paramsId)
+		public TabPageShorts(string title, string paramsId, string urlSuffix = null)
 		{
 			Title = title;
 			ParamsId = paramsId;
+			UrlSuffix = urlSuffix;
 		}
 	}
 
 	public class TabPageLive : YouTubeChannelTabPage
 	{
-		public TabPageLive(string title, string paramsId)
+		public TabPageLive(string title, string paramsId, string urlSuffix = null)
 		{
 			Title = title;
 			ParamsId = paramsId;
+			UrlSuffix = urlSuffix;
 		}
 	}
 
 	public class TabPagePlaylists : YouTubeChannelTabPage
 	{
-		public TabPagePlaylists(string title, string paramsId)
+		public TabPagePlaylists(string title, string paramsId, string urlSuffix = null)
 		{
 			Title = title;
 			ParamsId = paramsId;
+			UrlSuffix = urlSuffix;
 		}
 	}
 
 	public class TabPageCommunity : YouTubeChannelTabPage
 	{
-		public TabPageCommunity(string title, string paramsId)
+		public TabPageCommunity(string title, string paramsId, string urlSuffix = null)
 		{
 			Title = title;
 			ParamsId = paramsId;
+			UrlSuffix = urlSuffix;
 		}
 	}
 
 	public class TabPageChannels : YouTubeChannelTabPage
 	{
-		public TabPageChannels(string title, string paramsId)
+		public TabPageChannels(string title, string paramsId, string urlSuffix = null)
 		{
 			Title = title;
 			ParamsId = paramsId;
+			UrlSuffix = urlSuffix;
 		}
 	}
 
 	public class TabPageAbout : YouTubeChannelTabPage
 	{
-		public TabPageAbout(string title, string paramsId)
+		public TabPageAbout(string title, string paramsId, string urlSuffix = null)
 		{
 			Title = title;
 			ParamsId = paramsId;
+			UrlSuffix = urlSuffix;
 		}
 	}
 }
