e3e92a9ef6ee8f1f4c0717485102b0e07020ec15
BlackRain <masaraksh86@mail.ru>
2025-02-25 14:25:10 +0700

    Создан класс "YouTubeChannelWebPage"

---
+++ YouTubeApiLib/YouTubeChannelWebPage.cs
@@ -0,0 +1,20 @@
+﻿using MultiThreadedDownloaderLib;
+
+namespace YouTubeApiLib
+{
+	public class YouTubeChannelWebPage : InternetWebPage
+	{
+		public static int GetChannelTabWebPageCode(string channelId, YouTubeChannelTabPage channelTabPage,
+			out string response, FileDownloader downloader = null)
+		{
+			string url = channelTabPage.GetWebPageUrl(channelId);
+			return DownloadWebPageCode(url, out response, downloader);
+		}
+
+		public static int GetChannelTabWebPageCode(YouTubeChannel channel, YouTubeChannelTabPage channelTabPage,
+			out string response, FileDownloader downloader = null)
+		{
+			return GetChannelTabWebPageCode(channel.Id, channelTabPage, out response, downloader);
+		}
+	}
+}
