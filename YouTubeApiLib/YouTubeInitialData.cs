c6b1001c2499fd64e85ab9968fef5994ad272a84
BlackRain <masaraksh86@mail.ru>
2025-03-10 15:23:33 +0700

    Дополнен класс "YouTubeInitialData" Добавлен метод "ToVideoIdPage()" Добавлено свойство "DataWasReceivedWithContinuationToken" Обновлён конструктор класса

--- YouTubeApiLib/YouTubeInitialData.cs
+++ YouTubeApiLib/YouTubeInitialData.cs
@@ -6,17 +6,26 @@ namespace YouTubeApiLib
 	{
 		public JObject Data { get; }
 		public string RawData { get; }
+		public bool DataWasReceivedWithContinuationToken { get; }
 
-		public YouTubeInitialData(string rawData)
+		public YouTubeInitialData(string rawData, bool dataWasReceivedWithContinuationToken = false)
 		{
 			RawData = rawData;
 			Data = Utils.TryParseJson(rawData);
+			DataWasReceivedWithContinuationToken = dataWasReceivedWithContinuationToken;
 		}
 
 		public static YouTubeInitialData ExtractFromWebPageCode(string webPageCode,
 			string pattern = @"var ytInitialData =\s*(.*}}});</script")
 		{
 			return Utils.ExtractYouTubeInitialDataFromWebPageCode(webPageCode, pattern);
+		}
+
+		public YouTubeVideoIdPageResult ToVideoIdPage()
+		{
+			YouTubeVideoIdPage videoIdPage = new YouTubeVideoIdPage(RawData, DataWasReceivedWithContinuationToken);
+			int errorCode = videoIdPage.Parse() > 0 ? 200 : 404;
+			return new YouTubeVideoIdPageResult(videoIdPage, errorCode);
 		}
 	}
 }
