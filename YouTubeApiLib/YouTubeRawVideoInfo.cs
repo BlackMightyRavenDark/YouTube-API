e3119d9581b42c6b1c296d9681b6fb0453be89f3
BlackRain <masaraksh86@mail.ru>
2025-06-13 17:56:10 +0700

    Исправлена ещё одна ошибка в методе "YouTubeRawVideoInfo.Simplify()"

--- YouTubeApiLib/YouTubeRawVideoInfo.cs
+++ YouTubeApiLib/YouTubeRawVideoInfo.cs
@@ -68,7 +68,7 @@ namespace YouTubeApiLib
 		public YouTubeSimplifiedVideoInfoResult Simplify(JObject customMicroformat,
 			YouTubeStreamingData customStreamingData)
 		{
-			return SimplifyRawVideoInfo(VideoDetails, customMicroformat ?? Microformat, customStreamingData);
+			return SimplifyRawVideoInfo(VideoDetails, customMicroformat ?? Microformat, customStreamingData ?? StreamingData?.Data);
 		}
 
 		public YouTubeSimplifiedVideoInfoResult Simplify(JObject customMicroformat)
