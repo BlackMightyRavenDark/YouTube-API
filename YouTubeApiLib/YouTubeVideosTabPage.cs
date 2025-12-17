53453dbadbe8897f3aeda2f7d284ebe9a7c8fda4
BlackRain <masaraksh86@mail.ru>
2025-12-17 16:14:21 +0700

    [Lib] Добавлены перегрузки метода "YouTubeVideosTabPage.UpdateVideosMultiThreaded()"

--- YouTubeApiLib/YouTubeVideosTabPage.cs
+++ YouTubeApiLib/YouTubeVideosTabPage.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using MultiThreadedDownloaderLib;
 
 namespace YouTubeApiLib
 {
@@ -35,21 +37,32 @@ namespace YouTubeApiLib
 			return VideoList != null;
 		}
 
-		public bool UpdateVideosMultiThreaded()
+		public bool UpdateVideosMultiThreaded(byte simultaneousThreads,
+			CancellationToken cancellationToken, FileDownloader downloader = null)
 		{
 			if (IdList != null && IdList.Count > 0)
 			{
+				if (simultaneousThreads < 1) { simultaneousThreads = 2; }
 				ConcurrentBag<YouTubeVideo> bag = new ConcurrentBag<YouTubeVideo>();
-				var tasks = IdList.Select(videoId => Task.Run(() =>
-				{
-					YouTubeVideo video = YouTubeVideo.GetById(videoId);
-					if (video != null)
-					{
-						bag.Add(video);
-					}
-				}));
 
-				Task.WhenAll(tasks).Wait();
+				for (int i = 0; i < IdList.Count && !cancellationToken.IsCancellationRequested; i += simultaneousThreads)
+				{
+					int remaining = IdList.Count - i;
+					int count = remaining > simultaneousThreads ? simultaneousThreads : remaining;
+					if (count > 0)
+					{
+						List<string> idGroup = IdList.GetRange(i, count);
+						if (idGroup.Count > 0)
+						{
+							var tasks = idGroup.Select(videoId => Task.Run(() =>
+							{
+								YouTubeVideo video = YouTubeVideo.GetById(videoId, downloader);
+								if (video != null) { bag.Add(video); }
+							}));
+							Task.WhenAll(tasks).Wait();
+						}
+					}
+				}
 
 				if (bag.Count > 0)
 				{
@@ -62,6 +75,16 @@ namespace YouTubeApiLib
 			}
 
 			return VideoList != null;
+		}
+
+		public bool UpdateVideosMultiThreaded(byte simultaneousThreads)
+		{
+			return UpdateVideosMultiThreaded(simultaneousThreads, default);
+		}
+
+		public bool UpdateVideosMultiThreaded()
+		{
+			return UpdateVideosMultiThreaded(2);
 		}
 	}
 }
