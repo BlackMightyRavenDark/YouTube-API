98e3560c8ffaed3b01919575efe3aaabf911534f
BlackRain <masaraksh86@mail.ru>
2025-12-20 16:04:01 +0700

    [Lib] Добавлен метод "InternetWebPage.GetDefaultHttpHeaders()"

--- YouTubeApiLib/InternetWebPage.cs
+++ YouTubeApiLib/InternetWebPage.cs
@@ -9,27 +9,31 @@ namespace YouTubeApiLib
 		{
 			if (downloader == null)
 			{
-				WebHeaderCollection headers = new WebHeaderCollection()
-				{
-					{ "Host", "www.youtube.com" },
-					{ "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0" },
-					{ "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" },
-					{ "Accept-Language", "en-US" },
-					{ "Accept-Encoding", "gzip" },
-					{ "DNT", "1" },
-					{ "Sec-GPC", "1" },
-					{ "Upgrade-Insecure-Requests", "1" },
-					{ "Sec-Fetch-Dest", "document" },
-					{ "Sec-Fetch-Mode", "navigate" },
-					{ "Sec-Fetch-Site", "none" },
-					{ "Sec-Fetch-User", "?1" },
-					{ "Priority", "u=0, i" }
-				};
-
+				WebHeaderCollection headers = GetDefaultHttpHeaders();
 				downloader = new FileDownloader() { Headers = headers };
 			}
 
 			return Utils.DownloadString(url, out response, downloader);
+		}
+
+		public static WebHeaderCollection GetDefaultHttpHeaders()
+		{
+			return new WebHeaderCollection()
+			{
+				{ "Host", "www.youtube.com" },
+				{ "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0" },
+				{ "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" },
+				{ "Accept-Language", "en-US" },
+				{ "Accept-Encoding", "gzip" },
+				{ "DNT", "1" },
+				{ "Sec-GPC", "1" },
+				{ "Upgrade-Insecure-Requests", "1" },
+				{ "Sec-Fetch-Dest", "document" },
+				{ "Sec-Fetch-Mode", "navigate" },
+				{ "Sec-Fetch-Site", "none" },
+				{ "Sec-Fetch-User", "?1" },
+				{ "Priority", "u=0, i" }
+			};
 		}
 	}
 }
