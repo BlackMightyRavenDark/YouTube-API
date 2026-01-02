d63764ff00b31f608856566b6732372be3ede13b
BlackRain <masaraksh86@mail.ru>
2026-01-02 15:36:19 +0700

    [Lib] Добавлено свойство "YouTubeApiV1SearchResults.SearchQuery"

--- YouTubeApiLib/YouTubeApiV1SearchResults.cs
+++ YouTubeApiLib/YouTubeApiV1SearchResults.cs
@@ -5,17 +5,20 @@ namespace YouTubeApiLib
 	{
 		public string RawData { get; }
 		public YouTubeApiV1SearchResultFilter UsedFilter { get; }
+		public string SearchQuery { get; }
 		public bool IsContinuationItem { get; }
 		public int ErrorCode { get; }
 
 		public YouTubeApiV1SearchResults(
 			string rawData,
 			YouTubeApiV1SearchResultFilter usedFilter,
+			string searchQuery,
 			bool isContinuationItem,
 			int errorCode)
 		{
 			RawData = rawData;
 			UsedFilter = usedFilter;
+			SearchQuery = searchQuery;
 			IsContinuationItem = isContinuationItem;
 			ErrorCode = errorCode;
 		}
