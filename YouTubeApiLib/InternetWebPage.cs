f51a7253089d768f7eb7ac21a8d94bff39a23308
BlackRain <masaraksh86@mail.ru>
2026-06-23 15:48:30 +0700

    [Lib] Обновлены HTTP-заголовки в методе "InternetWebPage.GetDefaultHttpHeaders()"

--- YouTubeApiLib/InternetWebPage.cs
+++ YouTubeApiLib/InternetWebPage.cs
@@ -21,18 +21,16 @@ namespace YouTubeApiLib
 			return new WebHeaderCollection()
 			{
 				{ "Host", "www.youtube.com" },
-				{ "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0" },
+				{ "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0" },
 				{ "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" },
-				{ "Accept-Language", "en-US" },
-				{ "Accept-Encoding", "gzip" },
-				{ "DNT", "1" },
-				{ "Sec-GPC", "1" },
+				{ "Accept-Language", "en-US,en;q=0.9" },
+				{ "Accept-Encoding", "gzip, deflate, br, zstd" },
 				{ "Upgrade-Insecure-Requests", "1" },
 				{ "Sec-Fetch-Dest", "document" },
 				{ "Sec-Fetch-Mode", "navigate" },
 				{ "Sec-Fetch-Site", "none" },
 				{ "Sec-Fetch-User", "?1" },
-				{ "Priority", "u=0, i" }
+				{ "Connection", "keep-alive" }
 			};
 		}
 	}
